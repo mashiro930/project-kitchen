9cd6458c9754f30bcf68dc6e8d592f27ecc14846
mashiro930 <974196263@qq.com>
2024-10-24 05:36:43 +1100

    [Fix] Fix build error bugs

--- Project Kitchen/Assets/Scripts/UI/RecipeUI.cs
+++ Project Kitchen/Assets/Scripts/UI/RecipeUI.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using Image = UnityEngine.UI.Image;
 
 public class RecipeUI : MonoBehaviour
